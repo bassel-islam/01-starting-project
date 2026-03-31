4ef5ebd2c3a8770dc4eccf8ab6e1e10e8dbc56cc
bassel <bassel.islam@gmail.com>
2026-03-31 15:05:58 +0200

    cleaner dynamic way for user template.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -13,4 +13,7 @@ const randomUserIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 export class UserComponent {
   selectedUser=DUMMY_USERS[randomUserIndex];
   
+  get imagePath(){
+    return 'assets/users/'+this.selectedUser.avatar;
+  }
 }
