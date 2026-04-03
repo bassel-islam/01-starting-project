4cc536adca54557b8ccd02ad1b6505ac01743f26
bassel <bassel.islam@gmail.com>
2026-04-03 16:00:21 +0200

    Implement task management features: add new task component, enhance task completion handling, and update task display logic

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,19 +1,30 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { dummyTasks } from '../dummy-tasks';
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) userName?: string;
   @Input({ required: true }) userId?: string;
+  isAddingTask = false;
   tasks = dummyTasks;
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId);
+  }
+  onCompleteTask(taskId: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -21,9 +21,6 @@ import { User } from './user.model';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({required: true}) id!:string;
-  // @Input({required: true}) avatar!:string;
-  // @Input({required: true}) name!:string;
   @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>(); //or select = output<string>();
   @Input({ required: true }) selected!: boolean;
@@ -31,9 +28,6 @@ export class UserComponent {
     return 'assets/users/' + this.user.avatar;
   }
 
-  // avatar = input.required<string>();
-  // name = input.required<string>();
-  // imagePath = computed(() => 'assets/users/'+this.avatar());
   onSelectUser() {
     this.select.emit(this.user.id);
   }
